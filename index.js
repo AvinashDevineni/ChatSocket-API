559a0ac9dcb8328a9aa269964d94ed3711991937
Avinash Devineni <devinenia69420@gmail.com>
2024-08-15 15:27:20 -0500

    Implemented basic functionality: connecting to main server, which notifies user when a new room is created and when a person enters/leaves a room; connecting to rooms, where the user receives messages from other users and the server; and endpoints that allow creating a room and listing all rooms

---
+++ index.js
@@ -0,0 +1,181 @@
+const url = require('url');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.Server;
+
+function notifyPathedWss(pathedWSS, ...args) {
+    for (const ws of pathedWSS.wss.clients) {
+        ws.emit('message', ...args);
+        break;
+    }
+}
+
+//#region App & Server Initialization
+app.use(express.json());
+app.use(cors());
+
+app.post('/room/:roomName', (req, res) => {
+    const roomName = req.params.roomName;
+    const roomPath = `/room/${encodeURI(roomName.toLowerCase())}`;
+
+    for (const wss of wsRooms) {
+        if (wss.path === roomPath)
+            return res.status(400).json({ error: 'Room with same name already exists.' });
+    }
+
+    const wss = new PathedWebSocketServer(mainWss, roomPath, roomName);
+    wsRooms.push(wss);
+
+    const wssInfo = getPathedWssInfo(wss);
+    // notify about new room
+    notifyPathedWss(mainWss, new Blob([JSON.stringify(wssInfo)]));
+
+    res.json(wssInfo);
+});
+
+app.get('/room/:roomName', (req, res) => {
+    const roomName = req.params.roomName;
+    
+    let roomWss = null;
+    for (const wss of wsRooms) {
+        if (wss.name === roomName) {
+            roomWss = wss;
+            break;
+        }
+    }
+
+    if (roomWss == null)
+        return res.status(400).json({ error: `Room ${roomName} does not exist.` });
+
+    res.json(getPathedWssInfo(wss));
+});
+
+function getAllRooms() {
+    const rooms = [];
+    wsRooms.forEach(wss => rooms.push(getPathedWssInfo(wss)));
+    return rooms;
+}
+
+function getRoomMatchingPath(path) {
+    wsRooms.forEach(wss => {
+        if (wss.path === path)
+            return wss;
+    });
+
+    return null;
+}
+
+app.get('/room', (req, res) => {
+    if (req.body.path) {
+        const room = getRoomMatchingPath(req.body.path);
+        if (room === null)
+            return res.status(400).json({ 'error': 'Path given doesn not exist.' });
+
+        return res.json(getPathedWssInfo(room));
+    }
+
+    res.json({ rooms: getAllRooms() });
+});
+
+const server = app.listen(4000, () => {
+    console.log('Go to http://localhost:4000');
+});
+//#endregion
+
+//#region WSS Creation Definition
+function serverMessage(msg) {
+    return {
+        from: 'server',
+        message: msg
+    };
+}
+
+function getPathedWssInfo(pathedWSS) {
+    return {
+        name: pathedWSS.name,
+        path: pathedWSS.path,
+        numPeople: pathedWSS.wss.clients.size
+    };
+}
+
+function createServerlessWSS(wssOptions) {
+    const wss = new WebSocketServer({ noServer: true });
+    wss.on('connection', ws => {
+        ws.on('error', (e) => console.log(e));
+
+        ws.on('close', () => wssOptions.leaveAction(ws));
+
+        ws.on('message', msg => {
+            wss.clients.forEach(client => {
+                const isReady = client.readyState === WebSocket.OPEN;
+                if (wssOptions.shouldSendToSelf ? isReady : (client !== ws && isReady))
+                    client.send(msg);
+            });
+        });
+
+        wssOptions.joinAction(ws);
+    });
+
+    return wss;
+}
+
+class WssOptions {
+    constructor(joinAction = ws => ws.emit('message', new Blob([serverMessage('New Person Joined')])),
+                leaveAction = ws => ws.emit('message', new Blob([serverMessage('Person Left')])),
+                shouldSendToSelf = true) {
+        this.joinAction = joinAction;
+        this.leaveAction = leaveAction;
+        this.shouldSendToSelf = shouldSendToSelf;
+    }
+}
+
+class PathedWebSocketServer {
+    constructor(mainWss, path, name, serverOptions = new WssOptions(
+                    ws => {
+                        ws.emit('message', new Blob([JSON.stringify(serverMessage('New Person Joined'))]));
+                        notifyPathedWss(mainWss, new Blob([JSON.stringify(getPathedWssInfo(this))]));
+                    },
+                    ws => {
+                        ws.emit('message', new Blob([JSON.stringify(serverMessage('Person Left'))]));
+                        notifyPathedWss(mainWss, new Blob([JSON.stringify(getPathedWssInfo(this))]));
+                    }
+                ),
+                createNow = true) {
+        this.path = path;
+        this.name = name;
+
+        if (createNow)
+            this.wss = createServerlessWSS(serverOptions);
+    }
+
+    createWss(serverOptions = new WssOptions()) {
+        wss = createServerlessWSS(this.name, serverOptions);
+    }
+}
+//#endregion
+
+const mainWss = new PathedWebSocketServer(null, '/', 'Main', new WssOptions(ws => {}, ws => {}));
+let wsRooms = [new PathedWebSocketServer('', '', false)];
+wsRooms = [];
+
+server.on('upgrade', (req, socket, head) => {
+    const reqUrl = url.parse(req.url);
+    
+    if (reqUrl.pathname === mainWss.path) {
+        mainWss.wss.handleUpgrade(req, socket, head, ws => mainWss.wss.emit('connection', ws));
+        return;
+    }
+
+    for (const wss of wsRooms) {
+        if (reqUrl.pathname === wss.path) {
+            wss.wss.handleUpgrade(req, socket, head, ws => wss.wss.emit('connection', ws));
+            return;
+        }
+    }
+
+    console.log('Upgrade Failed\n');
+    
+    socket.destroy();
+});
